97f81e21b75427d72c28807593aee5fa308fd7c3
Athira Lakshmi P <athiralakshmi.prakash@gmail.com>
2023-02-19 18:18:47 +0530

    basic layout of todo App

--- sg.py
+++ sg.py
@@ -1,17 +1,23 @@
 import PySimpleGUI as sg
 
 feet_label = sg.Text("Enter the feet")
-inputbox1 = sg.Input()
+inputbox1 = sg.Input(key="feet_input")
 
 inches_label = sg.Text("Enter the inches")
-inptputbox2 = sg.Input()
+inptputbox2 = sg.Input(key="inches input")
 
-converter_button = sg.Button("Convert")
-output_box = sg.Output(78)
+converter_button = sg.Button("Convert",key=("converted value button "))
+output_box = sg.Output(key="output",text_color="green")
 
 window = sg.Window("converter", layout=[
     [feet_label, inputbox1],
     [inches_label, inptputbox2],
     [converter_button]])
-window.read()
+while True:
+
+    event,values=window.read()
+    print(event, value)
+
+
+
 window.close()
